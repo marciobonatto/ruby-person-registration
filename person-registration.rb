65fbb309c03facc9ae1941962b603b00308a371b
marciobonatto <marciobonattodsg@gmail.com>
2024-03-15 05:19:40 -0300

    Up first version

---
+++ person-registration.rb
@@ -0,0 +1,7 @@
+print "Escreva o seu PRIMEIRO NOME: "
+nome = gets.chomp
+print "Escreva o seu SOBRENOME: "
+sobrenome = gets.chomp
+print "Escreva a sua IDADE: "
+idade = gets.chomp.to_i
+puts "Nome completo: #{nome} #{sobrenome} | Idade: #{idade} anos"
